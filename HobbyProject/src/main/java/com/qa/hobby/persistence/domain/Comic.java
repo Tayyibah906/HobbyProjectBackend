ce01bcaf6d35231ee49ec2cd5251e532304ab538
tayyibah906 <tayyibah906@gmail.com>
2020-06-25 15:58:15 +0100

    completed persistence domains

--- HobbyProject/src/main/java/com/qa/hobby/persistence/domain/Comic.java
+++ HobbyProject/src/main/java/com/qa/hobby/persistence/domain/Comic.java
@@ -1,5 +1,103 @@
 package com.qa.hobby.persistence.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+
+@Entity
 public class Comic {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(name = "comic_title")
+	private String title;
+	
+	@Column
+	private String publisher;
+	
+	@Column
+	private String writer;
+	
+	@Column
+	private String coverArtist;
+	
+	@Column
+	private int issue;
+	
+	@ManyToOne(targetEntity = Universe.class)
+	private Universe universe;
+	
+	public Comic(String title, String writer, String publisher, String coverArtist, int issue) {
+		super();
+		this.title = title;
+		this.publisher = publisher;
+		this.coverArtist = coverArtist;
+		this.issue = issue;
+	}
+	
+	public Comic() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getCoverArtist() {
+		return coverArtist;
+	}
+
+	public void setCoverArtist(String coverArtist) {
+		this.coverArtist = coverArtist;
+	}
+
+	public int getIssue() {
+		return issue;
+	}
+
+	public void setIssue(int issue) {
+		this.issue = issue;
+	}
+
+	public Universe getUniverse() {
+		return universe;
+	}
+
+	public void setUniverse(Universe universe) {
+		this.universe = universe;
+	}
 
 }
