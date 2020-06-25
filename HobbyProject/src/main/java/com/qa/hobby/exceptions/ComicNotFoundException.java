9c1fa3116c37ef0da421d2cf9251040915d71571
tayyibah906 <tayyibah906@gmail.com>
2020-06-25 17:01:46 +0100

    Service and exceptions added

--- HobbyProject/src/main/java/com/qa/hobby/exceptions/ComicNotFoundException.java
+++ HobbyProject/src/main/java/com/qa/hobby/exceptions/ComicNotFoundException.java
@@ -1,5 +1,16 @@
 package com.qa.hobby.exceptions;
 
-public class ComicNotFoundException {
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason ="This comic does not exist")
+public class ComicNotFoundException extends EntityNotFoundException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 }
--- HobbyProject/src/main/java/com/qa/hobby/service/ComicService.java
+++ HobbyProject/src/main/java/com/qa/hobby/service/ComicService.java
@@ -1,5 +1,58 @@
 package com.qa.hobby.service;
 
-public class ComicService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
+import com.qa.hobby.exceptions.ComicNotFoundException;
+import com.qa.hobby.persistence.domain.Comic;
+import com.qa.hobby.persistence.repo.ComicRepo;
+
+@Service
+public class ComicService {
+	
+	private ComicRepo repo;
+	
+	public ComicService(ComicRepo repo) {
+		super();
+		this.repo = repo;
+	}
+	
+//	INSERT INTO comic VALUES...
+	public Comic create(Comic comic) {
+		return this.repo.save(comic);
+	}
+
+//	SELECT * FROM comic
+	public List<Comic> read(){
+		return this.repo.findAll();
+	}
+
+//	SELECT FROM comic WHERE id =
+	public Comic read(long id) {
+		return this.repo.findById(id).orElseThrow(() -> new ComicNotFoundException());
+	}
+	
+//	UPDATE comic SET ....
+	public Comic update(Comic comic, long id) {
+		Optional<Comic> optComic = this.repo.findById(id);
+		
+		Comic toUpdate = optComic.orElseThrow(() -> new ComicNotFoundException());
+		
+		toUpdate.setTitle(comic.getTitle());
+		toUpdate.setPublisher(comic.getPublisher());
+		toUpdate.setWriter(comic.getWriter());
+		toUpdate.setCoverArtist(comic.getCoverArtist());
+		toUpdate.setIssue(comic.getIssue());
+		
+		return this.repo.save(toUpdate);
+	}
+	
+	
+//	DELETE FROM comic WHERE id = 
+	public boolean delete(long id) {
+		this.repo.deleteById(id);
+		return this.repo.existsById(id);
+	}
 }
