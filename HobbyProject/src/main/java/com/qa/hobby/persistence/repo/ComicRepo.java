104eaac3aee73332652e2738d753123e541a331b
tayyibah906 <tayyibah906@gmail.com>
2020-06-25 16:11:47 +0100

    added repo comic and universe

--- HobbyProject/src/main/java/com/qa/hobby/persistence/repo/ComicRepo.java
+++ HobbyProject/src/main/java/com/qa/hobby/persistence/repo/ComicRepo.java
@@ -1,5 +1,14 @@
 package com.qa.hobby.persistence.repo;
 
-public class ComicRepo {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.hobby.persistence.domain.Comic;
+
+@Repository
+public interface ComicRepo extends JpaRepository<Comic, Long>{
+
+	List<Comic> findBytitle(String Title);
 }
--- HobbyProject/src/main/java/com/qa/hobby/persistence/repo/UniverseRepo.java
+++ HobbyProject/src/main/java/com/qa/hobby/persistence/repo/UniverseRepo.java
@@ -1,5 +1,12 @@
 package com.qa.hobby.persistence.repo;
 
-public class UniverseRepo {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.qa.hobby.persistence.domain.Universe;
+
+public interface UniverseRepo extends JpaRepository<Universe, Long> {
+
+	List<Universe> findByName(String Name);
 }
