d6f93248e6a11430ea9fec1ee0a55a712152b5ed
tayyibah906 <tayyibah906@gmail.com>
2020-07-15 17:12:49 +0100

    updated request.js url

--- HobbyProject/src/main/resources/static/JS/requests.js
+++ HobbyProject/src/main/resources/static/JS/requests.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const params = new URLSearchParams(window.location.search);
 const URL = '';
 //http://localhost:8080
@@ -6,14 +5,6 @@ const URL = '';
 function postComic() {
     document.getElementById("comicForm").addEventListener("submit", function(event){
       debugger
-=======
-const requestData = XMLHttpRequest();
-const params = new URLSearchParams(window.location.search);
-const BASE_URL = "http://localhost:8080"
-
-function postComic() {
-    document.getElementById("comicForm").addEventListener("submit", function(event)){
->>>>>>> 71d5ef02330a1fca406b8a1f397d42e3842491f6
         event.preventDefault();
         const comic = {};
         comic.title = this.title.value;
@@ -21,29 +12,16 @@ function postComic() {
         comic.artist = this.artist.value;
         comic.publisher = this.publisher.value;
         comic.issue = this.issue.value;
-<<<<<<< HEAD
         axios.post(URL + "/comic/createComic", comic)
             .then(response => output.innerText = res.comic)
             .catch(error => console.log(error));
     });
     
-=======
-        axios.post(`${BASE_URL}/hobby/createComic`, comic)
-            .then(response => output.innerText = res.comic)
-            .catch(error => console.log(error));
-    };
-    let insertmsg = document.querySelector(".insertmsg");
-    getMsg(flag, insertmsg);
->>>>>>> 71d5ef02330a1fca406b8a1f397d42e3842491f6
 };
 
 function getComic() {
     const comicOutput = document.getElementById('comicOutput');
-<<<<<<< HEAD
     axios.get(URL +'/comic/getAll')
-=======
-    axios.get(`${BASE_URL}/hobby/getAll`)
->>>>>>> 71d5ef02330a1fca406b8a1f397d42e3842491f6
         .then(res => {
             comicOutput.innerText = "";
             res.data.forEach((comic, i) => {
@@ -71,11 +49,7 @@ function makeElement(eleType, text, appendTo) {
 };
 
 function putComic() {
-<<<<<<< HEAD
     document.getElementById("comicForm").addEventListener("submit", function(event){
-=======
-    document.getElementById("comicForm").addEventListener("submit", function(event)){
->>>>>>> 71d5ef02330a1fca406b8a1f397d42e3842491f6
         event.preventDefault();
         const comic = {};
         comic.title = this.title.value;
@@ -83,27 +57,16 @@ function putComic() {
         comic.artist = this.artist.value;
         comic.publisher = this.publisher.value;
         comic.issue = this.issue.value;
-<<<<<<< HEAD
         axios.put(URL + "/comic/updateComic", comic)
             .then(response => output.innerText = res.comic)
             .catch(error => console.log(error));
     });
-=======
-        axios.put(`${BASE_URL}/hobby/updateComic`, comic)
-            .then(response => output.innerText = res.comic)
-            .catch(error => console.log(error));
-    };
->>>>>>> 71d5ef02330a1fca406b8a1f397d42e3842491f6
     let updatemsg = document.querySelector(".updatemsg");
       getMsg(get, updatemsg);
 };
 
 document.getElementById('delete-button').addEventListener("click", function(){
-<<<<<<< HEAD
     axios.delete(URL + "/comic/deleteComic" + document.getElementById("input-delete").value)
-=======
-    axios.delete(`${BASE_URL}/deleteComic` + document.getElementById("input-delete").value)
->>>>>>> 71d5ef02330a1fca406b8a1f397d42e3842491f6
     .then(response => output.innerText = res.comic)
     .catch(error => console.log(error));
 
@@ -111,62 +74,6 @@ document.getElementById('delete-button').addEventListener("click", function(){
   getMsg(true, deletemsg);
 });
 
-<<<<<<< HEAD
 postComic();
 getComic();
 putComic();
-=======
-
-
-function postUniverse() {
-    document.getElementById("universeForm").addEventListener("submit", function(event)){
-        event.preventDefault();
-        const universe = {};
-        universe.name = this.name.value;
-        axios.post(`${BASE_URL}/hobby/createUniverse`, universe)
-            .then(response => output.innerText = res.universe)
-            .catch(error => console.log(error));
-    };
-
-function getUniverse() {
-    const universeOutput = document.getElementById('universeOutput');
-    axios.get(`${BASE_URL}/hobby/getAllUniverse`)
-        .then(res => {
-            universeOutput.innerText = "";
-            res.data.forEach((universe, i) => {
-                const universeDiv = makeElement('div', '', universeOutput);
-                universeDiv.id = 'universe' + i;
-                universeDiv.addEventListener('click', function () {
-                    window.location = './index.html?id=' + universe.id;
-                });
-                makeElement('p', universe.name, universeDiv);
-            });
-        })
-        .catch(err => console.log(err));
-};
-
-function makeElement(eleType, text, appendTo) {
-    const element = document.createElement(eleType);
-    element.innerText = text;
-    appendTo.appendChild(element);
-    return element;
-};
-
-function putUniverse() {
-    document.getElementById("universeForm").addEventListener("submit", function(event)){
-        event.preventDefault();
-        const universe = {};
-        universe.name = this.name.value;
-        axios.put(`${BASE_URL}/hobby/updateUniverse`, universe)
-            .then(response => output.innerText = res.universe)
-            .catch(error => console.log(error));
-    };
-
-document.getElementById('delete-button').addEventListener("click", function(){
-    axios.delete(`${BASE_URL}/deleteUniverse` + document.getElementById("input-delete").value)
-    .then(response => output.innerText = res.universe)
-    .catch(error => console.log(error));
-  })
- }
-}
->>>>>>> 71d5ef02330a1fca406b8a1f397d42e3842491f6
